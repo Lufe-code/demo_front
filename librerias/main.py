173df63ee1812ed4594e4bcbb368d6274a3406e0
Lufe-code <luisfelipecruzcastellanos@gmail.com>
2025-02-17 14:15:07 -0500

    commit 2

--- librerias/main.py
+++ librerias/main.py
@@ -1,1 +1,10 @@
 print("hola")
+
+# Pedir al usuario un número
+numero = int(input("Ingrese un número: "))
+
+# Verificar si es menor o igual a 10
+if numero <= 10:
+    print("Sí")
+else:
+    print("No")
